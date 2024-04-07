83bc95d307fc2a77c0d2d39306fb56fdeacca333
RachelAlcraft <rachelalcraft@gmail.com>
2024-04-07 17:41:40 +0100

    remove file tinker

--- .github/scripts/release.py
+++ .github/scripts/release.py
@@ -43,10 +43,6 @@ def create_new_patch_release():
         check=True,
     )
 
-    f = open("demofile2.txt", "a")
-    f.write("Now the file has more content!")
-    f.close()
-
 
 if __name__ == "__main__":
     create_new_patch_release()
