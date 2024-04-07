7dc646c139ec291ae2290b7866d9f7c3cc099ae8
RachelAlcraft <rachelalcraft@gmail.com>
2024-04-07 17:34:44 +0100

    messing

--- .github/scripts/release.py
+++ .github/scripts/release.py
@@ -43,6 +43,10 @@ def create_new_patch_release():
         check=True,
     )
 
+    f = open("demofile2.txt", "a")
+    f.write("Now the file has more content!")
+    f.close()
+
 
 if __name__ == "__main__":
     create_new_patch_release()
