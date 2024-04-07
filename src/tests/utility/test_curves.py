c78b336567538d05c80d7a56b31e833cd4b87f5c
RachelAlcraft <rachelalcraft@gmail.com>
2024-04-07 20:27:01 +0100

    import

--- src/tests/utility/test_curves.py
+++ src/tests/utility/test_curves.py
@@ -4,10 +4,6 @@ import inspect
 from pathlib import Path
 
 sys.path.append(os.path.join(os.path.dirname(Path(__file__).parent.parent), ""))
-
-print(sys.path)
-
-
 from app_lib_py import ClassCurves as cc
 
 
